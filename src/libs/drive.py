e716a9bf83928e6f6793ef20f59190f230c8e09c
Lie Lu <lielu2011@live.com>
2024-10-08 01:27:43 -0500

    Resolved conflict

--- src/libs/drive.py
+++ src/libs/drive.py
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-def straight(motor, speed, distance=None, degrees=None, seconds=None):
-    """
-    Control a drive motor for FLL Spike.
-    
-    Args:
-        motor: The motor object to control.
-        speed (int): The speed of the motor (-100 to 100).
-        distance (float, optional): The distance to travel in centimeters.
-        degrees (int, optional): The number of degrees to rotate the motor.
-        seconds (float, optional): The number of seconds to run the motor.
-    """
-    if distance is not None:
-        # Assuming a standard wheel diameter of 5.6 cm for LEGO wheels
-        # Circumference = pi * diameter
-        wheel_circumference = 3.14159 * 5.6
-        # Calculate degrees to rotate based on distance
-        degrees_to_rotate = (distance / wheel_circumference) * 360
-        motor.run_for_degrees(int(degrees_to_rotate), speed)
-    elif degrees is not None:
-        motor.run_for_degrees(degrees, speed)
-    elif seconds is not None:
-        motor.run_for_seconds(seconds, speed)
-    else:
-        motor.start(speed)
-
-    motor.wait_until_not_moving()
-
-
-=======
 from hub import port, motion_sensor
 import motor #imports a ability to control a single motor
 import motor_pair #imports a pair of motors moving like one
@@ -77,4 +47,3 @@ def turnLeft(degrees):
         current_yaw_position = motion_sensor.tilt_angles()[0]
 
 turnRight(90)
->>>>>>> 6826e273ce5fd4df1d34f75a58fdc819ffbdcc4b
