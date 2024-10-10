37a0bdaab5da975ac7ed2ef24c359081245fb056
121114 <31lue@smtexas.org>
2024-10-09 20:08:47 -0500

    Update drive ports

--- src/libs/drive.py
+++ src/libs/drive.py
@@ -6,28 +6,30 @@ distanceInDegrees = 0
 current_yaw_position = 0
 
 def init():
-    motor_pair.pair(motor_pair.PAIR_1, port.F, port.E) #Motor pair called motor_pair.PAIR_1, left motor: port A, right motor: port B
+    motor_pair.pair(motor_pair.PAIR_1, port.D, port.C) #Motor pair called motor_pair.PAIR_1, left motor: port A, right motor: port B
     motion_sensor.reset_yaw(0) #Makes the robot think its facing forward.
 
 def forward(distance, speed): #FUNCTION
     init()
     global current_yaw_position, distanceInDegrees
-    distanceInDegrees = distance / 28 * 360
+    distanceInDegrees = distance / 17.5 * 360
     current_yaw_position = motion_sensor.tilt_angles()[0]
-    motor.reset_relative_position(port.E, 0) 
-    while motor.relative_position(port.E) < distanceInDegrees:
+    motor.reset_relative_position(port.C, 0) 
+    while motor.relative_position(port.C) < distanceInDegrees:
         motor_pair.move_for_degrees(motor_pair.PAIR_1, 5, current_yaw_position * -3, velocity=int(round(speed/100.0*660.0)))
+        if ValueError:
+            motor_pair.move_for_degrees(motor_pair.PAIR_1, 5, current_yaw_position * -1, velocity=int(round(speed/100.0*660.0)))
         current_yaw_position = motion_sensor.tilt_angles()[0]
     motor_pair.stop(motor_pair.PAIR_1)
 
 def backward(distance, speed): #FUNCTION
     motion_sensor.reset_yaw(0)
-    motor_pair.pair(motor_pair.PAIR_1, port.E, port.F)
+    motor_pair.pair(motor_pair.PAIR_1, port.D, port.C)
     global current_yaw_position, distanceInDegrees
-    distanceInDegrees = distance / 28 * 360
+    distanceInDegrees = distance / 17.5 * 360
     current_yaw_position = motion_sensor.tilt_angles()[0]
-    motor.reset_relative_position(port.E, 0)
-    while motor.relative_position(port.E) < distanceInDegrees:
+    motor.reset_relative_position(port.C, 0)
+    while motor.relative_position(port.C) < distanceInDegrees:
         motor_pair.move_for_degrees(motor_pair.PAIR_1, 5, current_yaw_position * -3, velocity=int(round(speed/100.0*660.0)))
         current_yaw_position = motion_sensor.tilt_angles()[0]
     motor_pair.stop(motor_pair.PAIR_1)
@@ -46,4 +48,4 @@ def turnLeft(degrees):
         motor_pair.move_for_degrees(motor_pair.PAIR_1, -1, 100)
         current_yaw_position = motion_sensor.tilt_angles()[0]
 
-turnRight(90)
+forward(20, 50)
