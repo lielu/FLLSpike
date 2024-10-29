8a4bee60c0cb94d79f5fa289c37e9bc640ca41e2
121114 <31lue@smtexas.org>
2024-10-28 20:32:47 -0500

    Commit run_grey - mission 9 - put squid in cold seep

--- src/libs/run_grey.py
+++ src/libs/run_grey.py
@@ -1,12 +1,30 @@
-import runloop
+import runloop, time
 from attachment import *
 from drive import *
 
 from hub import port
 
 async def run_grey():
-    await forward(20, 50)
-    await test_attachment()
+    await forward(10, 50)
+    await turnLeft(42)
+    await forward(40, 50)
+    time.sleep(0.5)
+    await backward(10, 50)
+    await turnLeft(48)
+    await forward(50, 50)
+    await turnRight(92)
+    await forward(24, 50)
+    await frontLeft(int(6.5 * 360), 100)
+    await backward(24, 50)
+    await frontRight(int(6.5*360), 100)
+    await turnLeft(92)
+    await backward(50, 100)
+    await turnRight(48)
+    await backward(40, 100)
+    import sys
+    sys.exit()
+
+
 
 if __name__ == "__main__":
     runloop.run(run_grey())
