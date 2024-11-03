f795585d037d9821b34d699452522a7f62fc28e7
Shenganator <31shengk@smtexas.org>
2024-11-03 10:42:41 -0600

    Full run of Mission 1 and mission 2

--- src/libs/run_red.py
+++ src/libs/run_red.py
@@ -1,10 +1,23 @@
 import runloop
 from drive import *
 
+
 from hub import port
 
 async def main():
-    await forward(10, 50)
+    await forward(50, 50)
+    await turnLeft(45)
+    await forward(37, 50)
+    await turnLeft(95)
+    await forward(30, 50)
+    await backward(15, 50)
+    await turnRight(30)
+    await forward(40, 100)
+    await backward(13, 50)
+    await turnRight(47)
+    await backward(45, 50)
+    await turnRight(55)
+    await backward (45, 50)
 
 if __name__ == "__main__":
     runloop.run(main())
