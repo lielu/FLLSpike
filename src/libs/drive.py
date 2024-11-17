bf16b13261405cce049bf905f1ed32a54de31199
Dylan Evans <dylan.evans@geodesoftware.com>
2024-11-17 17:14:37 -0600

    Drive by time code commit

--- src/libs/drive.py
+++ src/libs/drive.py
@@ -3,6 +3,34 @@ import motor #imports a ability to control a single motor
 import motor_pair #imports a pair of motors moving like one
 import runloop
 
+motor_speed = 1100
+async def movebydistance(distance, speed):
+    global current_yaw_position, distanceInDegrees # Global variables: available outside of function
+    distanceInDegrees = distance / 17.5 * 360#Sets how far the motor should turn in degrees
+    motor.reset_relative_position(port.C, 0) #Sets the relative position to 0 so we can track its rotations
+    while motor.relative_position(port.C) < distanceInDegrees: #repeats the following code until it has moved far enough
+        current_yaw_position = motion_sensor.tilt_angles()[0]#Sets yaw position to 0
+        adjusted = int(round(0.3 * current_yaw_position)) # tilt_angles are in decidegrees (degrees * 10)
+        #rotate by 60 degrees unless we're less than 60 degrees from target
+        rotation=60 if distanceInDegrees-motor.relative_position(port.C)> 60 else distanceInDegrees-motor.relative_position(port.C)
+        print(rotation)
+        try: #attempts to try this
+            await motor_pair.move_for_degrees(motor_pair.PAIR_1, int(rotation), adjusted, velocity=int(round(speed/100.0*motor_speed)), stop=motor.CONTINUE) #Move forward 60 degrees, check again
+        except(ValueError):#This runs if the robot finds that the value is too big(100, -100)
+            print('value error: ', adjusted)#Prints if finds error
+            await motor_pair.move_for_degrees(motor_pair.PAIR_1, int(rotation), int(round(adjusted / 3)), velocity=int(round(speed/100.0*motor_speed)), stop=motor.CONTINUE) # Reduces turn amount so no value error
+            # Goes to the line just under " while motor.relative…"
+
+async def movebytime(time, speed):
+    runtime=0
+    while runtime < time*1000: #repeats the following code until it has moved far enough
+        current_yaw_position = motion_sensor.tilt_angles()[0]#Sets yaw position to 0
+        runtime = runtime + 200
+        adjusted = int(round(0.3 * current_yaw_position)) # tilt_angles are in decidegrees (degrees * 10)
+        await motor_pair.move_for_time(motor_pair.PAIR_1, 200, adjusted, velocity=int(round(speed/100.0*motor_speed)), stop=motor.CONTINUE)
+    #motor_pair.stop(motor_pair.PAIR_1)
+
+
 distanceInDegrees = 0
 current_yaw_position = 0#Sets yaw position to 0
 
@@ -12,57 +40,52 @@ async def init_drive(port1, port2):
     motion_sensor.reset_yaw(0) #Makes the robot think its facing forward.
     runloop.until(motion_sensor.stable)
 
-async def forward(distance, speed): #FUNCTION
+async def forward(value, speed, mode = 'distance'): #FUNCTION
     await init_drive(port.D, port.C)#Waits for init_drive() to initialize
-    global current_yaw_position, distanceInDegrees # Global variables: available outside of function
-    distanceInDegrees = distance / 17.5 * 360#Sets how far the motor should turn in degrees
-    motor.reset_relative_position(port.C, 0) #Sets the relative position to 0 so we can track its rotations
-    while motor.relative_position(port.C) < distanceInDegrees: #repeats the following code until it has moved far enough
-        current_yaw_position = motion_sensor.tilt_angles()[0]#Sets yaw position to 0
-        adjusted = int(round(0.3 * current_yaw_position)) # tilt_angles are in decidegrees (degrees * 10)
-        try: #attempts to try this
-            motor_pair.move_for_degrees(motor_pair.PAIR_1, 60, adjusted, velocity=int(round(speed/100.0*660.0))) #Move forward 60 degrees, check again
-        except(ValueError):#This runs if the robot finds that the value is too big(100, -100)
-            print('value error: ', adjusted)#Prints if finds error
-            motor_pair.move_for_degrees(motor_pair.PAIR_1, 60, int(round(adjusted / 3)), velocity=int(round(speed/100.0*660.0))) # Reduces turn amount so no value error
-            # Goes to the line just under " while motor.relative…"
+    if mode == 'distance':
+        await movebydistance(value, speed)
+    else:
+        await movebytime(value, speed)
     motor_pair.stop(motor_pair.PAIR_1)#stops motor after done with distance
 
-async def backward(distance, speed): #Basically the same as the first code, but the motors are reversed, thus giving us reverse action
+async def backward(value, speed, mode='distance'): #Basically the same as the first code, but the motors are reversed, thus giving us reverse action
     await init_drive(port.C, port.D) # Reverse motors
-    global current_yaw_position, distanceInDegrees
-    distanceInDegrees = distance / 17.5 * 360
-    motor.reset_relative_position(port.C, 0)
-    while abs(motor.relative_position(port.C)) < distanceInDegrees:
-        current_yaw_position = motion_sensor.tilt_angles()[0]
-        adjusted = int(round(0.3 * current_yaw_position)) # tilt_angles are in decidegrees (degrees * 10)
-        try:
-            motor_pair.move_for_degrees(motor_pair.PAIR_1, 60, adjusted, velocity=int(round(speed/100.0*660.0)))
-        except(ValueError):
-            print('value error: ', adjusted)
-            motor_pair.move_for_degrees(motor_pair.PAIR_1, 60, int(round(adjusted / 3)), velocity=int(round(speed/100.0*660.0)))
-    motor_pair.stop(motor_pair.PAIR_1)
+    if mode == 'distance':
+        await movebydistance(value, speed)
+    else:
+        await movebytime(value, speed)
+    motor_pair.stop(motor_pair.PAIR_1)#stops motor after done with distance
 
-async def turnRight(degrees): #Function for turn right code
+async def turnbydegrees(degrees, mod):
+    current_yaw_position = abs(motion_sensor.tilt_angles()[0])
+    while current_yaw_position < degrees* 10:
+        #print(str(current_yaw_position) + ":" +str(degrees*10) )
+        await motor_pair.move_for_degrees(motor_pair.PAIR_1, mod, 100, stop=motor.CONTINUE)
+        current_yaw_position = abs(motion_sensor.tilt_angles()[0])
+
+async def turnbytime(time, mod):
+    await motor_pair.move_for_time(motor_pair.PAIR_1, time * 1000, mod * 100) #turn for small amount
+
+async def turnRight(value, mode='degrees'): #Function for turn right code
     await init_drive(port.D, port.C) # Waits for initiation
-    current_yaw_position = motion_sensor.tilt_angles()[0]# Refreshes variable "current_yaw_position"
-    while abs(current_yaw_position) < abs(degrees*10): #abs to make sure both are positive, < so that it stops when it is at the goal
-        print(str(degrees) + ", " + str(current_yaw_position)) #Check current degrees
-        motor_pair.move_for_degrees(motor_pair.PAIR_1, 10, 100) #turn for small amount
-        current_yaw_position = motion_sensor.tilt_angles()[0]# Refreshes variable "current_yaw_position"
+    if mode=='degrees':
+        await turnbydegrees(value, 1)
+    else:
+        await turnbytime(value, 1)
 
-async def turnLeft(degrees): #same as right, only left
+async def turnLeft(value, mode='degrees'): #same as right, only left
     await init_drive(port.D, port.C)
-    current_yaw_position = motion_sensor.tilt_angles()[0]# Refreshes variable "current_yaw_position"
-    while abs(current_yaw_position) < abs(degrees * 10):
-        motor_pair.move_for_degrees(motor_pair.PAIR_1, -10, 100)
-        current_yaw_position = motion_sensor.tilt_angles()[0]# Refreshes variable "current_yaw_position"
+    if mode=='degrees':
+        await turnbydegrees(value, -1)
+    else:
+        await turnbytime(value, -1)
+
 
 async def test_drive(): #test code for drive functions
-    await forward(50, 100)
-    await turnLeft(90)
-    await backward(50, 100)
-    await turnRight(90)
+
+    #await init_drive(port.D, port.C)
+    await turnRight(90, 'degrees')
+    #await motor_pair.move_for_time(motor_pair.PAIR_1, 5000, 0, velocity=int(round(100/100.0*1100.0)))
 
 if __name__ == "__main__":
     runloop.run(test_drive())
