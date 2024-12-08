784393f844c419a5a128d06e97c0274b939388cf
Dylan Evans <dylan.evans@geodesoftware.com>
2024-12-08 11:17:12 -0600

    both runs working, but need to fix return to home

--- src/libs/run_yellow.py
+++ src/libs/run_yellow.py
@@ -7,36 +7,45 @@ async def return_from_mission3():
     await backward(12, 20) #Starts to bring back to home
     await turnLeft(20)
     await backward(75, 100)
-    await frontRight(int(2 * 360), 75) 
+    await frontRight(int(2 * 360), 75)
 async def run_yellow():#Start right back piece on farthest dark line in red square
     #Left raises the arm
     #right lowers the arm
-    await frontLeft(int(1.8 * 360), 50) #Lowers arm
-    
-    await forward(65, 50) #Moves robot straight toward mission 3
-    await turnRight(30) #Turns first turn to straighten out robot toward mission 3
-    await forward(12, 20)
-    await frontRight(int(5 * 360), 75) #Lowers arm to solve mission
-    await frontLeft(int(3 * 360), 50) 
-    await backward(10, 35)
+    #### MOVE TO MISSION 3 ####
+    runloop.run(frontLeft(int(1.8 * 360), 50), forward(65, 50)) #Lowers arm
+
+    #await forward(65, 50) #Moves robot straight toward mission 3
+    await turnRight(28) #Turns first turn to straighten out robot toward mission 3
+    await forward(14, 20)#Slowly moves to mission 3
+    #### END MOVE TO MISSION 3 ####
+
+    #### SOLVE MISSION 3 ####
+    await frontRight(int(6 * 360), 75) #Lowers arm to solve mission
+    await frontLeft(int(3 * 360), 50) #Lifts arm not to bump into anything
+    await backward(8, 35) #Backs away from mission
+    #### END SOLVE MISSION 3 ####
+
     await turnRight(45) #Ligns up to solve mission 11
     await forward(70, 80)
     await turnRight(22)
-    await forward(30, 75)
-    await turnLeft(60)
+    await forward(35, 75)
+    await turnLeft(55)
     await backward(7, 20)
     await frontRight(int(3 * 360), 75) #Puts arm in position
+    #### READY TO SOLVE MISSION 11 ####
     await forward(7, 50)
-    await frontLeft(int(2 * 360), 60) #Lifts arm to solve one whale
+    await frontLeft(int(4 * 360), 60) #Lifts arm to solve one whale
     await backward(12, 40)
     await turnRight(45)
-    await forward(40, 75) 
-    await turnLeft(90)
-    await forward(5, 20) #Aligns to second whale
+    await forward(45, 75)
+    await turnLeft(80)
+    await forward(8, 20) #Aligns to second whale
     await turnLeft(25)
-    await frontRight(int(2 * 360), 75)
+    await frontRight(int(4 * 360), 75)
+    #### SOLVING 2ND PART OF MISSION 11 ####
+    await turnRight(45)
     await backward(50, 50) #Starting to go back to blue circle
-    #return_from_mission3()    
+    #return_from_mission3()
 
 
 if __name__ == "__main__":
