1860695cde10df4729a2633086a7883f723b00e0
Nathaniel Shou <nathanielshou@ChatGPT-53.local>
2025-01-05 14:18:18 -0600

    run_blue code to solve submarine

--- src/runtime/run_blue_lib.py
+++ src/runtime/run_blue_lib.py
@@ -1,18 +1,26 @@
 
 # Generated code
 export_code: str = """
-import runloop
-from drive import *
+import runloop, time
 from attachment import *
+from drive import *
 
 from hub import port
 
-async def main():
+async def run_blue():
+    await forward(47, 100)
+    await turnLeft(49)
+    await forward(61, 100)
     await forward(10, 50)
-    await frontRight(-720, 100)
+    await frontLeft(int(6.5 * 360), 100)
+    await backward(5, 80)
+    await turnLeft(2)
+    await backward(63, 100)
+    await turnRight(48)
+    await backward(50, 100)
 
 if __name__ == "__main__":
-    runloop.run(main())
+    runloop.run(run_blue())
     import sys
     sys.exit()
 """
