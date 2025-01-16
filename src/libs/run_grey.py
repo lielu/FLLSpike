c09e2e8fff5770a561455260562a04b2ed0ca82e
121114 <31lue@smtexas.org>
2025-01-16 16:34:05 -0600

    Optimized + finalized run grey code

--- src/libs/run_grey.py
+++ src/libs/run_grey.py
@@ -7,60 +7,33 @@ from hub import port, button
 
 
 async def run_grey():
-    #COLLECTION + Squid: STARTS AT THE SECOND TO LAST THIN LINE FROM THE RIGHT
-    await frontLeft(int(70*2), 100)
-    runloop.run(frontLeft(int(80*1.2), 100), forward(25, 50))
-    await forward(30, 50)
-    await frontRight(int(150*1.2), 100)
-    await turnRight(15)
-    await frontLeft(int(150*1.2), 100)
-    await forward(13, 100)
-    await frontRight(int(360*2), 100)
-    await backward(30, 100)
-    await turnLeft(45)
-    await backward(55, 100)
-
     # Navigate to squid
-    await frontLeft(int(70*1.2), 100)
-    runloop.run(forward(25, 50), frontLeft(int(80*1.2)))
+    await frontLeft(2*360, 100)
+    runloop.run(forward(30, 50), frontLeft(int(80*1.2), 100))
+    await frontRight(2*360, 100)
     await turnLeft(45)
 
     # Drop squid into basket
-    await forward(14, 15)
-    time.sleep(0.5)
+    await forward(12, 20)
 
-    # Navigate home
-    await frontRight(2*360, 100)
-    await backward(20, 100)
-    await turnRight(45)
-    await frontRight(1*360, 100)
-    await backward(30, 100)
-
-    # Wait to let person add move to start position
-    while not button.pressed(button.LEFT):
-        pass
-
-    await forward(30, 50)
-    await turnLeft(30)
-    await forward(40, 50)
+    #COLLECTION + Squid: STARTS AT THE SECOND TO LAST THIN LINE FROM THE RIGHT
+    await frontRight(140, 100)
+    await backward(20, 50)
     await turnRight(30)
-    await forward(8, 50)
-    await turnRight(70)
-    await frontLeft(int(6.5*360), 100)
-    await forward(10, 100)
-    await frontRight(7*360, 100)
-    await backward(15, 100)
-    await turnRight(90)
-    await forward(40, 100)
-    await turnLeft(30, 100)
-    await froward(40, 100)
-
-    #START WITH LEFT BUMBER ON SECOND TO LAST THIN LINE FROM RIGHT
-    while not button.pressed(button.LEFT):
-        pass
-
+    await frontLeft(2*360, 100)
+    await forward(40, 50)
+    await frontRight(360, 100)
+    await turnRight(45)
+    await frontLeft(140, 100)
+    await forward(7, 50)
+    await frontRight(4*360, 100)
+    await backward(30, 100)
+    await turnLeft(45)
+    await frontRight(360, 100)
+    await backward(30, 100)
+    await turnLeft(50)
+    await backward(40, 100)
     
-
     import sys
     sys.exit()
 
