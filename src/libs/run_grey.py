e90d98ea4f035e7a1f867d549e844ffd863309b0
121114 <31lue@smtexas.org>
2025-01-16 20:32:39 -0600

    Made run_grey faster

--- src/libs/run_grey.py
+++ src/libs/run_grey.py
@@ -9,30 +9,28 @@ from hub import port, button
 async def run_grey():
     # Navigate to squid
     await frontLeft(2*360, 100)
-    runloop.run(forward(30, 50), frontLeft(int(80*1.2), 100))
+    runloop.run(forward(30, 100), frontLeft(int(80*1.2), 100))
     await frontRight(2*360, 100)
     await turnLeft(50)
 
     # Drop squid into basket
-    await forward(9, 20)
+    await forward(6, 30)
 
     #COLLECTION + Squid: STARTS AT THE SECOND TO LAST THIN LINE FROM THE RIGHT
     await frontRight(140, 100)
-    await backward(20, 50)
-    await turnRight(30)
+    await backward(20, 100)
+    await turnRight(35)
     await frontLeft(2*360, 100)
-    await forward(40, 50)
+    await forward(35, 50)
     await frontRight(360, 100)
-    await turnRight(45)
-    await frontLeft(140, 100)
+    await turnRight(35)
+    await frontLeft(2*360, 100)
     await forward(6, 50)
     await frontRight(4*360, 100)
     await backward(10, 100)
     await frontRight(360, 100)
-    await turnRight(70)
-    await backward(30, 100)
-    await turnLeft(100)
-    await backward(40, 100)
+    await turnLeft(60)
+    await backward(70, 100)
     
     import sys
     sys.exit()
