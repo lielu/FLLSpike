f88d19f82196b8cb7b0218bd4b1817d8ab3ad362
121114 <31lue@smtexas.org>
2025-01-19 17:07:38 -0600

    Merged after qualifier

--- src/runtime/run_grey_lib.py
+++ src/runtime/run_grey_lib.py
@@ -10,17 +10,31 @@ from hub import port, button
 
 
 async def run_grey():
-    # Navigate to squid
+    '''# Navigate to squid
     await frontLeft(2*360, 100)
     runloop.run(forward(30, 100), frontLeft(int(80*1.2), 100))
     await frontRight(2*360, 100)
     await turnLeft(50)
 
     # Drop squid into basket
-    await forward(6, 30)
+    await forward(6, 30)'''
 
     #COLLECTION + Squid: STARTS AT THE SECOND TO LAST THIN LINE FROM THE RIGHT
-    await frontRight(140, 100)
+    await frontLeft(2*360, 100)
+    await forward(55, 100)
+    await turnRight(15)
+    await forward(10, 100)
+    await frontRight(4*360, 100)
+    await backward(5, 100)
+    await turnLeft(15)
+    await frontRight(200, 100)
+    await backward(40, 100)
+    await turnLeft(40)
+    await forward(20, 50) 
+    await backward(40, 100)
+    import sys
+    sys.exit()
+    '''return
     await backward(20, 100)
     await turnRight(35)
     await frontLeft(2*360, 100)
@@ -32,10 +46,9 @@ async def run_grey():
     await backward(10, 100)
     await frontRight(360, 100)
     await turnLeft(60)
-    await backward(70, 100)
+    await backward(70, 100)'''
     
-    import sys
-    sys.exit()
+    
 
 async def food():
     await frontLeft(45, 100)
