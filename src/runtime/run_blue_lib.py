6767691cb7d0981ab91d1b74e0e4507d3bb7eaf0
Lie Lu <lielu2011@live.com>
2025-01-26 11:12:32 -0600

    Adjusting submarine and anglerfish run_blue, achieving ~90% consistency

--- src/runtime/run_blue_lib.py
+++ src/runtime/run_blue_lib.py
@@ -8,19 +8,18 @@ from drive import *
 from hub import port
 #Robot left edge aligned with fourth thin line from left, end pushing to the wall#
 async def run_blue():
-    await forward(40, 100)
-    await turnLeft(50)
+    await forward(42, 100)
+    await turnLeft(49)
     await forward(61, 100)
-    await forward(10, 50)
+    await forward(5, 20)
     #arives at the submarine
     await frontLeft(int(6.7 * 360), 100)
-    time.sleep(2)
-    # await frontLeft(int(-6.7 * 360), 100)
-    await backward(7, 80)
+    time.sleep(2) # keep the submarine in position
+    runloop.run(frontLeft(int(-6.7 * 360), 100), backward(9, 80)) # back off and lower arm at the same time
     await turnLeft(67)
     await forward(25, 70)
     await turnRight(65)
-    await backward(2, 100)
+    # await backward(2, 100) # doesn't seem like robot needs to move
     #finshes anglerfish
 
 
