d9733c21dade6575747e7d77eaf957582d0b4384
Lie Lu <lielu2011@live.com>
2025-02-07 10:54:18 -0600

    Updated run_blue and run_green_lib

--- src/libs/run_blue.py
+++ src/libs/run_blue.py
@@ -5,17 +5,18 @@ from drive import *
 from hub import port
 #Robot left edge aligned with fourth thin line from left, end pushing to the wall#
 async def run_blue():
-    await forward(40, 100)
-    await turnLeft(50)
-    await forward(61, 100)
-    await forward(10, 50)
+    await forward(42, 100)
+    await turnLeft(49)
+    await forward(53, 100)
+    await forward(10, 20)
     #arives at the submarine
-    await frontLeft(int(6.9 * 360), 100)
-    await backward(7, 80)
+    await frontLeft(int(6.7 * 360), 100)
+    time.sleep(2) # keep the submarine in position
+    runloop.run(frontLeft(int(-6.7 * 360), 100), backward(9, 80)) # back off and lower arm at the same time
     await turnLeft(67)
-    await forward(28, 100)
+    await forward(20, 70)
     await turnRight(65)
-    await backward(4, 50)
+    # await backward(2, 100) # doesn't seem like robot needs to move
     #finshes anglerfish
 
 
