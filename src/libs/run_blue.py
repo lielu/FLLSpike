6e2dfae3310f1d2146fede9a059a049c602a8ad1
Lie Lu <lielu2011@live.com>
2025-02-07 17:44:58 -0600

    Updated run_blue with forwardbytime

--- src/libs/run_blue.py
+++ src/libs/run_blue.py
@@ -8,13 +8,13 @@ async def run_blue():
     await forward(42, 100)
     await turnLeft(49)
     await forward(53, 100)
-    await forward(10, 20)
+    await forward(1, 40, mode = "time")
     #arives at the submarine
     await frontLeft(int(6.7 * 360), 100)
     time.sleep(2) # keep the submarine in position
-    runloop.run(frontLeft(int(-6.7 * 360), 100), backward(9, 80)) # back off and lower arm at the same time
+    runloop.run(frontLeft(int(-2 * 360), 100), backward(9, 80)) # back off and lower arm at the same time
     await turnLeft(67)
-    await forward(20, 70)
+    await forward(23, 70)
     await turnRight(65)
     # await backward(2, 100) # doesn't seem like robot needs to move
     #finshes anglerfish
