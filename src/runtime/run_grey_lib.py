516fc1cf651e72933e94bf655c5808dfb4a07463
Lie Lu <lielu2011@live.com>
2025-02-07 17:11:31 -0600

    Updated run_grey

--- src/runtime/run_grey_lib.py
+++ src/runtime/run_grey_lib.py
@@ -13,30 +13,31 @@ async def run_grey():
     
     #COLLECTION + Squid: STARTS AT FIRST THIN LINE AFTER SECOND BOLD LINE FROM THE RIGHT, CLAW ALL THE WAY OPEN
     #BEGIN COLLECT KRILL
-    await forward(40, 100)
+    await forward(37, 100)
     await turnLeft(15)
     await forward(20, 80)
     await frontRight(720, 100)
     await turnRight(55)
     await frontLeft(720, 100)
-    await forward(4, 100)
+    await forward(5, 100)
     #CLOSE CLAW TO SECURE KRILL
     await frontRight(720, 100)
     #NAVIGATE TO SONAR DISCOVERY
-    await backward(5, 100)
-    await turnLeft(40)
-    await forward(16, 60)
+    await backward(1, 100)
+    await turnLeft(35)
+    time.sleep(0.08)
+    await forward(1, 60, mode = 'time')
     #LOWER ARM
     await rearRight(370, 100)
     #MOVE BACK TO SOLVE SONAR DISCOVERY
     await backward(25, 40)
     #NAVIGATE TO OCTOPUS
-    runloop.run(rearLeft(362, 100), frontRight(100, 100), backward(35, 90))
-    await turnLeft(45)
+    runloop.run(rearLeft(362, 100), frontRight(100, 100), backward(33, 90))
+    await turnLeft(55)
     #SOLVE OCTOPUS
-    await forward(15, 40)
+    await forward(1, 50, mode = 'time')
     #BRING OCTOPUS HOME
-    await backward(70, 100)
+    await backward(55, 100)
 
     while not button.pressed(button.LEFT):
         motion_sensor.reset_yaw(0)
